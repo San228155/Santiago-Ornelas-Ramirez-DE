6e237c19233188d18060b46213ddb8e7d64c8774
Santi228155 <santi4.linebacker@gmail.com>
2026-03-01 18:06:01 +0000

    hope

--- bike_data_set/src/bike_data_project/drivers/silver_driver.py
+++ bike_data_set/src/bike_data_project/drivers/silver_driver.py
@@ -1,5 +1,3 @@
-from pyspark.sql import SparkSession
-
 from bike_data_project.config.meta_driven_transformations import TRANSFORMATION
 from bike_data_project.transformations.silver_clean import clean_data_tables
 from bike_data_project.transformations.silver_preprocessed import preprocess_data_tables
@@ -7,6 +5,9 @@ from bike_data_project.transformations.silver_upload import (
     silver_table_surrogate,
     silver_table_upload,
 )
+from pyspark.sql import SparkSession
+
+
 def get_spark():
     spark = SparkSession.getActiveSession()
     if spark is None:
