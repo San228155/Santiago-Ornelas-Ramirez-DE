6aa04620f08800b85cc3a8736da4ee14bea41fd1
Santi228155 <santi4.linebacker@gmail.com>
2026-03-02 03:56:27 +0000

    hopefully get some logging

--- bike_data_set/src/bike_data_project/drivers/bronze_driver.py
+++ bike_data_set/src/bike_data_project/drivers/bronze_driver.py
@@ -1,3 +1,5 @@
+import logging
+
 from pyspark.sql import SparkSession
 
 from bike_data_project.config.meta_driven_ingestion import INGESTION
@@ -6,6 +8,9 @@ from bike_data_project.runner.bronze_runner import ingestion
 catalog = "bike_data_lakehouse"
 ingestion_schema = "raw_data"
 output_schema = "bronze"
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def get_spark():
     spark = SparkSession.getActiveSession()
@@ -18,6 +23,7 @@ def main():
 
     for volume, table_dicts in INGESTION.items():
         for clean_table_name, config_dict in table_dicts.items():
+            logger.info(f"materializing table {clean_table_name}")
             ingestion(spark= spark, catalog = catalog, ingestion_schema= ingestion_schema, output_schema= output_schema, volume=volume, clean_table_name=clean_table_name, config_dict=config_dict)
 
 if __name__ == "__main__":
