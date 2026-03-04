892420f369b4bebdb14b88bf26ed073e97ba829f
Santi228155 <santi4.linebacker@gmail.com>
2026-03-04 19:24:17 +0000

    maybe?

--- bike_data_set/src/bike_data_project/config/meta_driven_ingestion.py
+++ bike_data_set/src/bike_data_project/config/meta_driven_ingestion.py
@@ -1,34 +1,34 @@
 INGESTION = {
     "source_crm": {
         "crm_customer_info":{
-                "source":"cust_info", #file_name
+                "source":"cust_info",
                 "table_type": "CSV",
                 "allow_overwrite": True
              },
         "crm_product_info": {
-                "source": "prd_info", #file_name
+                "source": "prd_info",
                 "table_type": "CSV",
                 "allow_overwrite": True
             },
         "crm_sales_info": {
-                "source": "sales_details", #file_name
+                "source": "sales_details",
                 "table_type": "CSV",
                 "allow_overwrite": True
             }
     },
     "source_erp": {
             "erp_customer_az_12": {
-                "source": "CUST_AZ12", #file_name
+                "source": "CUST_AZ12",
                 "table_type": "CSV",
                 "allow_overwrite": True
             },
             "erp_location_a101": {
-                "source": "LOC_A101", #file_name
+                "source": "LOC_A101",
                 "table_type": "CSV",
                 "allow_overwrite": True
             },
             "erp_product_category_g1_v2": {
-                "source": "PX_CAT_G1V2", #file_name
+                "source": "PX_CAT_G1V2",
                 "table_type": "CSV",
                 "allow_overwrite": True
             }
